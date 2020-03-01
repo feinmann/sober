ec9925bb7ca2b54d6bd5ff0edd9e15d738a9fbd1
macbd <bidmaster@gmx.de>
2020-03-01 11:01:50 +0100

    Deactivate folder app/www since it is empty at the moment
    Can be used for favicon for example

--- R/app_ui.R
+++ R/app_ui.R
@@ -14,13 +14,13 @@ app_ui <- function() {
 #' @import shiny
 golem_add_external_resources <- function(){
   
-  addResourcePath(
-    'www', system.file('app/www', package = 'sober')
-  )
+  #addResourcePath(
+  #  'www', system.file('app/www', package = 'sober')
+  #)
  
   tags$head(
-    golem::activate_js(),
-    golem::favicon()
+    #golem::activate_js()#,
+    #golem::favicon()
     # Add here all the external resources
     # If you have a custom.css in the inst/app/www
     # Or for example, you can add shinyalert::useShinyalert() here
